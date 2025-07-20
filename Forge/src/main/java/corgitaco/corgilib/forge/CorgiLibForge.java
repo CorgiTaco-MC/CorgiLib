1746b22c17e07a8f20ebd98a916a7a55e68b1822
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-07-20 19:22:22 -0400

    Move Forge Register Commands event into main

--- Forge/src/main/java/corgitaco/corgilib/forge/CorgiLibForge.java
+++ Forge/src/main/java/corgitaco/corgilib/forge/CorgiLibForge.java
@@ -3,6 +3,8 @@ package corgitaco.corgilib.forge;
 import corgitaco.corgilib.CorgiLib;
 import corgitaco.corgilib.forge.network.ForgeNetworkHandler;
 import corgitaco.corgilib.forge.platform.ForgePlatform;
+import corgitaco.corgilib.server.commands.CorgiLibCommands;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -14,11 +16,16 @@ public class CorgiLibForge {
     public CorgiLibForge() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::registerCommands);
         CorgiLib.init();
         ForgePlatform.CACHED.values().forEach(deferredRegister -> deferredRegister.register(modEventBus));
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
         ForgeNetworkHandler.init();
+    }
+
+    private void registerCommands(final RegisterCommandsEvent event) {
+        CorgiLibCommands.registerCommands(event.getDispatcher(), event.getBuildContext());
     }
 }
