52b2323c599596d512b69f596cff19dc240e5264
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2025-07-20 19:57:01 -0400

    Fix Commands Not being Registered on NeoForge

--- Forge/src/main/java/corgitaco/corgilib/forge/CorgiLibForge.java
+++ Forge/src/main/java/corgitaco/corgilib/forge/CorgiLibForge.java
@@ -3,6 +3,8 @@ package corgitaco.corgilib.forge;
 import corgitaco.corgilib.CorgiLib;
 import corgitaco.corgilib.forge.network.ForgeNetworkHandler;
 import corgitaco.corgilib.forge.platform.ForgePlatform;
+import corgitaco.corgilib.server.commands.CorgiLibCommands;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
@@ -14,13 +16,10 @@ public class CorgiLibForge {
 
     public CorgiLibForge(final FMLJavaModLoadingContext context) {
         IEventBus modEventBus = context.getModEventBus();
-        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener((FMLCommonSetupEvent event) -> ForgeNetworkHandler.init());
         CorgiLib.init();
         ForgePlatform.CACHED.values().forEach(deferredRegister -> deferredRegister.register(modEventBus));
         modEventBus.<DataPackRegistryEvent.NewRegistry>addListener(newRegistry -> ForgePlatform.DATAPACK_REGISTRIES.forEach(newRegistryConsumer -> newRegistryConsumer.accept(newRegistry)));
-    }
-
-    private void commonSetup(final FMLCommonSetupEvent event) {
-        ForgeNetworkHandler.init();
+        modEventBus.addListener((RegisterCommandsEvent event) -> CorgiLibCommands.registerCommands(event.getDispatcher(), event.getBuildContext()));
     }
 }
